a8a00d5e54cd3620e31f4d232bff38951b60de53
abdouseye <abdou.gsietechnology.com>
2026-06-23 23:16:18 +0000

    refactor(updated alembic/env.py)

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,6 @@ from logging.config import fileConfig
 from app.core.config import settings
 from app.core.database import Base
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
@@ -68,8 +67,8 @@ def run_migrations_online() -> None:
 
 def do_run_migrations(connection):    
     context.configure(
-            connection=connection,
-            target_metadata=target_metadata
+        connection=connection,
+        target_metadata=target_metadata
     )
 
     with context.begin_transaction():
